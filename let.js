5745e68939bd25a4c530f553c8cea40d1545c77a
AmbarItzel <ambarcruz898@gmail.com>
2025-07-31 15:56:10 -0600

    Add files via upload

---
+++ let.js
@@ -0,0 +1,23 @@
+/* Let es una palabra reservada  que se ocupa para declarar variables
+por ejemplo;
+Se usa para declarar una variable que puede cambiar su valor más adelante.
+*/
+
+//Definir variables 
+
+let numero = 5;
+
+let horaSalida = 10;
+
+let edaad = 16;
+
+let nombre = "Ámbar";
+nombre = "Ambar12";
+
+/* De la siguiente manera no se puede vover a declarar la variable 
+a la que ya se le asigno un valor en l mismo bloque de codigo
+
+let a = 5;
+let a = 10;
+*/
+
